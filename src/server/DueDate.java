dd8e6b9217be41b5b7ea28fc733db9b0d80582a9
HP <HP@DESKTOP-F5N0HK7>
2020-12-10 19:29:04 +0100

    DueDate

--- src/server/DueDate.java
+++ src/server/DueDate.java
@@ -1,5 +1,23 @@
 package server;
 
-public class DueDate {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
+public class DueDate extends ToDo{
+	private DateTimeFormatter formatter;
+	
+	public DueDate(String title, String description, int ID, Priority priority) {
+		super (title, description, ID, priority);
+	}
+	
+	public LocalDate setDate() {
+	LocalDate date = LocalDate.now();
+	String text = date.format(formatter);
+	LocalDate parsedDate = LocalDate.parse(text, formatter);
+	
+	return parsedDate;
+	 
+	
+
+}
 }
