c007d5e8d85ad8ea1284d8e2ec8777655befec3e
toni-schaeublin <70199973+toni-schaeublin@users.noreply.github.com>
2020-12-10 19:50:55 +0100

    ...

--- src/server/DueDate.java
+++ src/server/DueDate.java
@@ -5,16 +5,17 @@ import java.time.format.DateTimeFormatter;
 
 public class DueDate extends ToDo{
 	private DateTimeFormatter formatter;
+	private LocalDate date;
 	
-	public DueDate(String title, String description, int ID, Priority priority) {
+	public DueDate(String title, String description, int ID, Priority priority, LocalDate date) {
 		super (title, description, ID, priority);
+		this.date=date;
 	}
 	
-	public LocalDate setDate() {
-	LocalDate date = LocalDate.now();
-	String text = date.format(formatter);
+	public LocalDate getDate() {
+	//LocalDate date = LocalDate.now();
+	String text = this.date.format(formatter);
 	LocalDate parsedDate = LocalDate.parse(text, formatter);
-	
 	return parsedDate;
 	 
 	
