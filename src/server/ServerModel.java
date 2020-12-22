e314291c32a70cd19ba65a17691d620dab293808
toni-schaeublin <70199973+toni-schaeublin@users.noreply.github.com>
2020-12-22 11:41:39 +0100

    Empfang von Nachrichten in eigenen Thread (Runnable) verpackt...

--- src/server/ServerModel.java
+++ src/server/ServerModel.java
@@ -1,6 +1,7 @@
 package server;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
@@ -27,98 +28,113 @@ public class ServerModel extends Thread {
 				try (Socket socket = listener.accept();
 						BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 						OutputStreamWriter out = new OutputStreamWriter(socket.getOutputStream());) {
-					String message = in.readLine();
-					/*
-					 * Message vom Client in Teile zerlegen: MessageType, token, Data, Data, Data,
-					 * Data... und in Array speichern
-					 * 
-					 * Folgende Teile müssen vom Client gesendet werden:
-					 * 
-					 * CreateLogin, Username, Password Login, Username, Password ChangePasword,
-					 * token, New password Logout, - CreateToDo, token, Title, Priority,
-					 * Description, DueDate GetToDo, token, ID DeleteToDo, token, ID ListToDos,
-					 * token, List of IDs Ping, (Token)
-					 */
+					// Thread zum Lesen von einkommenden Nachrichten
+					Runnable r = new Runnable() {
+						@Override
+						public void run() {
+							String message = "";
+							while (message != null) {
+								try {
+									message = in.readLine();
+									/*
+									 * Message vom Client in Teile zerlegen: MessageType, token, Data, Data, Data,
+									 * Data... und in Array speichern
+									 * 
+									 * Folgende Teile müssen vom Client gesendet werden:
+									 * 
+									 * CreateLogin, Username, Password Login, Username, Password ChangePasword,
+									 * token, New password Logout, - CreateToDo, token, Title, Priority,
+									 * Description, DueDate GetToDo, token, ID DeleteToDo, token, ID ListToDos,
+									 * token, List of IDs Ping, (Token)
+									 */
+									String[] messageParts = message.split(SEPARATOR);
+									String messageType = messageParts[0];
+									switch (messageType) {
+									case "CreateLogin":
+										int size = messageParts.length;
+										if (size == 3) {
+											if (createLogin(messageParts[1], messageParts[2])
+													&& Checker.checkEmail(messageParts[1])
+													&& Checker.checkPassword(messageParts[2])) {
+												reply = "Result|true";
+											}
+										} else {
+											reply = "Result|false";
+										}
+										break;
+									case "Login":
+										int sizeLogin = messageParts.length;
+										if (sizeLogin == 3) {
+											if (login(messageParts[1], messageParts[2])) {
+												reply = "Result|true|" + accounts.getUser(messageParts[1]).getToken();
+											}
+										} else {
+											reply = "Result|false";
+										}
+										break;
+									case "ChangePassword":
+										int sizePassword = messageParts.length;
+										if (sizePassword == 3) {
+											if (changePassword(messageParts[1], messageParts[2])
+													&& Checker.checkPassword(messageParts[2])) {
+												reply = "Result|true";
+											}
+										} else {
+											reply = "Result|false";
+										}
 
-					String[] messageParts = message.split(SEPARATOR);
-					String messageType = messageParts[0];
-					switch (messageType) {
-					case "CreateLogin":
-						int size = messageParts.length;
-						if (size == 3) {
-							if (createLogin(messageParts[1], messageParts[2]) && Checker.checkEmail(messageParts[1])
-									&& Checker.checkPassword(messageParts[2])) {
-								reply = "Result|true";
+										break;
+									case "Logout":
+										int sizeLogout = messageParts.length;
+										if (sizeLogout == 3) {
+											logout(messageParts[1], messageParts[2]);
+										} else {
+											reply = "Result|false";
+										}
+
+										break;
+									case "CreateToDo":
+										int sizeCreate = messageParts.length;
+										if (sizeCreate == 6) {
+											// createToDo(messageParts[2], messageParts[3], messageParts[4],
+											// messageParts[5]);
+										}
+
+										break;
+									case "DeleteToDo":
+										int sizeDelete = messageParts.length;
+										if (sizeDelete == 3) {
+											// deleteToDo(messageParts[3]);
+										}
+
+										break;
+									case "ListToDos":
+										int sizeList = messageParts.length;
+										if (sizeList == 3) {
+											// ListToDo(messageParts [3]);
+										}
+										System.out.println("ListToDos");
+										break;
+									case "Ping":
+										System.out.println("Ping");
+										break;
+									default:
+										out.write("Result|false");
+										out.flush();
+									}
+									//reply wird in den jeweiligen Methoden des Switchteils erstellt...
+									out.write(reply);
+									out.flush();
+									
+									
+									
+								} catch (IOException e) {
+									message = null; // end loop if we have a communications error
+								}
 							}
-						} else {
-							reply = "Result|false";
 						}
-						break;
-					case "Login":
-						int sizeLogin = messageParts.length;
-						if (sizeLogin == 3) {
-							if (login(messageParts[1], messageParts[2])) {
-								reply = "Result|true|" + accounts.getUser(messageParts[1]).getToken();
-							}
-						} else {
-							reply = "Result|false";
-						}
-						break;
-					case "ChangePassword":
-						int sizePassword = messageParts.length;
-						if (sizePassword == 3) {
-							if (changePassword(messageParts[1], messageParts[2])
-									&& Checker.checkPassword(messageParts[2])) {
-								reply = "Result|true";
-							}
-						} else {
-							reply = "Result|false";
-						}
+					};
 
-						break;
-					case "Logout":
-						int sizeLogout = messageParts.length;
-						if (sizeLogout == 3) {
-							logout(messageParts[1], messageParts[2]);
-						} else {
-							reply = "Result|false";
-						}
-
-						break;
-					case "CreateToDo":
-						int sizeCreate = messageParts.length;
-						if (sizeCreate == 6) {
-							// createToDo(messageParts[2], messageParts[3], messageParts[4],
-							// messageParts[5]);
-						}
-
-						break;
-					case "DeleteToDo":
-						int sizeDelete = messageParts.length;
-						if (sizeDelete == 3) {
-							// deleteToDo(messageParts[3]);
-						}
-
-						break;
-					case "ListToDos":
-						int sizeList = messageParts.length;
-						if (sizeList == 3) {
-							// ListToDo(messageParts [3]);
-						}
-						System.out.println("ListToDos");
-						break;
-					case "Ping":
-						System.out.println("Ping");
-						break;
-					default:
-						out.write("false\n");
-						out.flush();
-						System.out.println("false\n");
-					}
-//reply wird in den jeweiligen Methoden des Switchteils erstellt...
-					String reply = ("");
-					out.write(reply);
-					out.flush();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
