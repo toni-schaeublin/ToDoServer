f91ffce81a408a7cb363f32d30ce426f626bdddb
toni-schaeublin <70199973+toni-schaeublin@users.noreply.github.com>
2020-12-24 13:14:11 +0100

    ...

--- src/server/ServerModelClientThread.java
+++ src/server/ServerModelClientThread.java
@@ -2,9 +2,7 @@ package server;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 
