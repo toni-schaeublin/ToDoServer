e4ce70c1957d0639408cbfcc5053feb3e376ea51
toni-schaeublin <70199973+toni-schaeublin@users.noreply.github.com>
2021-01-06 15:21:48 +0100

    Nullpointer Exc. auf dem Server beim Schliessen des Clients gelöst.

--- src/server/ServerModelClientThread.java
+++ src/server/ServerModelClientThread.java
@@ -48,178 +48,180 @@ public class ServerModelClientThread extends Thread {
 				 * Description, DueDate GetToDo, token, ID DeleteToDo, token, ID ListToDos,
 				 * token, List of IDs Ping, (Token)
 				 */
-				String[] messageParts = message.split(SEPARATOR, -1);
-				String messageType = messageParts[0];
-				switch (messageType) {
-				case "CreateLogin":
-					int size = messageParts.length;
-					if (size == 3 && Checker.freeUsername(messageParts[1], this.accounts)
-							&& Checker.checkEmail(messageParts[1]) && Checker.checkPassword(messageParts[2])) {
-						if (createLogin(messageParts[1], messageParts[2])) {
-							reply = "Result|true\n";
-							server.saveAccounts();
-						} else {
-							reply = "Result|false\n";
-						}
-					} else {
-						reply = "Result|false\n";
-					}
-					break;
-				case "Login":
-					int sizeLogin = messageParts.length;
-					if (sizeLogin == 3) {
-						if (login(messageParts[1], messageParts[2])) {
-							reply = "Result|true|" + accounts.getUser(messageParts[1]).getToken() + "\n";
-							loggedIn = true;
-						} else {
-							reply = "Result|false\n";
-						}
-					} else {
-						reply = "Result|false\n";
-					}
-					break;
-				case "ChangePassword":
-					int sizePassword = messageParts.length;
-					if (sizePassword == 3 && Checker.checkPassword(messageParts[2])) {
-						if (changePassword(messageParts[1], messageParts[2])) {
-							reply = "Result|true\n";
-							server.saveAccounts();
-						} else {
-							reply = "Result|false\n";
-						}
-					} else {
-						reply = "Result|false\n";
-					}
-
-					break;
-				case "Logout":
-					int sizeLogout = messageParts.length;
-					if (sizeLogout == 2) {
-						if (logout(messageParts[1]) && this.loggedIn) {
-							reply = "Result|true\n";
-							loggedIn = false;
-						} else {
-							reply = "Result|false\n";
-						}
-					} else {
-						reply = "Result|false\n";
-					}
-
-					break;
-				case "CreateToDo":
-					int sizeCreate = messageParts.length;
-					if (sizeCreate == 5 && Checker.checkStringIsBetween(3, 20, messageParts[2])
-							&& Checker.checkPriority(messageParts[3])
-							&& Checker.checkStringIsBetween(0, 255, messageParts[4])) {
-						if (createToDo(messageParts[1], messageParts[2], messageParts[3], messageParts[4])) {
-							reply = "Result|true|" + this.specificID + "\n";
-							server.saveToDos();
-						} else {
-							reply = "Result|false\n";
-						}
-					} else {
-						reply = "Result|false\n";
-					}
-
-					break;
-				case "GetToDo":
-					int sizeGet = messageParts.length;
-					if (sizeGet == 3) {
-						try {
-							int id = Integer.parseInt(messageParts[2]);
-							if (getToDo(messageParts[1], id)) {
-								String returnId = Integer.toString(toDo.getID());
-								String title = toDo.getTitle();
-								String priority = toDo.getPriority();
-								String description = toDo.getDescription();
-								String dueDate = "";
-								Boolean withDueDate = false;
-								try {
-									dueDate = toDo.getDueDate();
-									withDueDate = true;
-								} catch (Exception e) {
-									System.out.println("ToDo ohne dueDate");
-									withDueDate = false;
-								}
-								if (withDueDate) {
-									reply = "Result|true|" + returnId + "|" + title + "|" + priority + "|" + description
-											+ "|" + dueDate + "\n";
-								} else {
-									reply = "Result|true|" + returnId + "|" + title + "|" + priority + "|" + description
-											+ "\n";
-								}
+				if (message != null) {
+					String[] messageParts = message.split(SEPARATOR, -1);
+					String messageType = messageParts[0];
+					switch (messageType) {
+					case "CreateLogin":
+						int size = messageParts.length;
+						if (size == 3 && Checker.freeUsername(messageParts[1], this.accounts)
+								&& Checker.checkEmail(messageParts[1]) && Checker.checkPassword(messageParts[2])) {
+							if (createLogin(messageParts[1], messageParts[2])) {
+								reply = "Result|true\n";
+								server.saveAccounts();
 							} else {
 								reply = "Result|false\n";
 							}
-						} catch (Exception e) {
-							System.out.println("Da ist etwas schief gelaufen! " + e);
+						} else {
 							reply = "Result|false\n";
 						}
-					} else {
-						reply = "Result|false\n";
-					}
-					break;
-				case "DeleteToDo":
-					int sizeDelete = messageParts.length;
-					if (sizeDelete == 3) {
-						try {
-							int id = Integer.parseInt(messageParts[2]);
-							if (deleteToDo(messageParts[1], id)) {
+						break;
+					case "Login":
+						int sizeLogin = messageParts.length;
+						if (sizeLogin == 3) {
+							if (login(messageParts[1], messageParts[2])) {
+								reply = "Result|true|" + accounts.getUser(messageParts[1]).getToken() + "\n";
+								loggedIn = true;
+							} else {
+								reply = "Result|false\n";
+							}
+						} else {
+							reply = "Result|false\n";
+						}
+						break;
+					case "ChangePassword":
+						int sizePassword = messageParts.length;
+						if (sizePassword == 3 && Checker.checkPassword(messageParts[2])) {
+							if (changePassword(messageParts[1], messageParts[2])) {
 								reply = "Result|true\n";
+								server.saveAccounts();
+							} else {
+								reply = "Result|false\n";
+							}
+						} else {
+							reply = "Result|false\n";
+						}
+
+						break;
+					case "Logout":
+						int sizeLogout = messageParts.length;
+						if (sizeLogout == 2) {
+							if (logout(messageParts[1]) && this.loggedIn) {
+								reply = "Result|true\n";
+								loggedIn = false;
+							} else {
+								reply = "Result|false\n";
+							}
+						} else {
+							reply = "Result|false\n";
+						}
+
+						break;
+					case "CreateToDo":
+						int sizeCreate = messageParts.length;
+						if (sizeCreate == 5 && Checker.checkStringIsBetween(3, 20, messageParts[2])
+								&& Checker.checkPriority(messageParts[3])
+								&& Checker.checkStringIsBetween(0, 255, messageParts[4])) {
+							if (createToDo(messageParts[1], messageParts[2], messageParts[3], messageParts[4])) {
+								reply = "Result|true|" + this.specificID + "\n";
 								server.saveToDos();
 							} else {
 								reply = "Result|false\n";
 							}
-						} catch (Exception e) {
-							System.out.println("Da ist etwas schief gelaufen! " + e);
+						} else {
 							reply = "Result|false\n";
 						}
-					} else {
-						reply = "Result|false\n";
-					}
-					break;
-				case "ListToDos":
-					int sizeList = messageParts.length;
-					if (sizeList == 2) {
-						try {
-							if (listToDos(messageParts[1])) {
-								reply = "Result|true";
-								for (ToDo t : this.toDos) {
-									String part = ("|" + t.getID());
-									reply = reply.concat(part);
+
+						break;
+					case "GetToDo":
+						int sizeGet = messageParts.length;
+						if (sizeGet == 3) {
+							try {
+								int id = Integer.parseInt(messageParts[2]);
+								if (getToDo(messageParts[1], id)) {
+									String returnId = Integer.toString(toDo.getID());
+									String title = toDo.getTitle();
+									String priority = toDo.getPriority();
+									String description = toDo.getDescription();
+									String dueDate = "";
+									Boolean withDueDate = false;
+									try {
+										dueDate = toDo.getDueDate();
+										withDueDate = true;
+									} catch (Exception e) {
+										System.out.println("ToDo ohne dueDate");
+										withDueDate = false;
+									}
+									if (withDueDate) {
+										reply = "Result|true|" + returnId + "|" + title + "|" + priority + "|"
+												+ description + "|" + dueDate + "\n";
+									} else {
+										reply = "Result|true|" + returnId + "|" + title + "|" + priority + "|"
+												+ description + "\n";
+									}
+								} else {
+									reply = "Result|false\n";
 								}
-								reply = reply.concat("\n");
-							} else {
+							} catch (Exception e) {
+								System.out.println("Da ist etwas schief gelaufen! " + e);
 								reply = "Result|false\n";
 							}
-						} catch (Exception e) {
-							System.out.println("Da ist etwas schief gelaufen! " + e);
+						} else {
 							reply = "Result|false\n";
 						}
-					} else {
+						break;
+					case "DeleteToDo":
+						int sizeDelete = messageParts.length;
+						if (sizeDelete == 3) {
+							try {
+								int id = Integer.parseInt(messageParts[2]);
+								if (deleteToDo(messageParts[1], id)) {
+									reply = "Result|true\n";
+									server.saveToDos();
+								} else {
+									reply = "Result|false\n";
+								}
+							} catch (Exception e) {
+								System.out.println("Da ist etwas schief gelaufen! " + e);
+								reply = "Result|false\n";
+							}
+						} else {
+							reply = "Result|false\n";
+						}
+						break;
+					case "ListToDos":
+						int sizeList = messageParts.length;
+						if (sizeList == 2) {
+							try {
+								if (listToDos(messageParts[1])) {
+									reply = "Result|true";
+									for (ToDo t : this.toDos) {
+										String part = ("|" + t.getID());
+										reply = reply.concat(part);
+									}
+									reply = reply.concat("\n");
+								} else {
+									reply = "Result|false\n";
+								}
+							} catch (Exception e) {
+								System.out.println("Da ist etwas schief gelaufen! " + e);
+								reply = "Result|false\n";
+							}
+						} else {
+							reply = "Result|false\n";
+						}
+						break;
+					case "Ping":
+						int sizePing = messageParts.length;
+						if (sizePing > 1 && loggedIn) {
+							try {
+								this.token = accounts.getUserFromToken(messageParts[1]).getToken();
+								reply = "Result|true\n";
+							} catch (Exception e) {
+								System.out.println("Da ist etwas schief gelaufen! " + e);
+								reply = "Result|false\n";
+							}
+						} else {
+							reply = "Result|true\n";
+						}
+						break;
+					default:
 						reply = "Result|false\n";
 					}
-					break;
-				case "Ping":
-					int sizePing = messageParts.length;
-					if (sizePing > 1 && loggedIn) {
-						try {
-							this.token = accounts.getUserFromToken(messageParts[1]).getToken();
-							reply = "Result|true\n";
-						} catch (Exception e) {
-							System.out.println("Da ist etwas schief gelaufen! " + e);
-							reply = "Result|false\n";
-						}
-					} else {
-						reply = "Result|true\n";
-					}
-					break;
-				default:
-					reply = "Result|false\n";
+					// reply wird in den jeweiligen Methoden des Switchteils erstellt...
+					out.write(reply);
+					out.flush();
 				}
-				// reply wird in den jeweiligen Methoden des Switchteils erstellt...
-				out.write(reply);
-				out.flush();
 			}
 		} catch (IOException e) {
 			System.out.println(e.toString());
@@ -305,7 +307,7 @@ public class ServerModelClientThread extends Thread {
 		if (size > 0) {
 			try {
 				ArrayList<ToDo> toDos = new ArrayList<>();
-				//Die ID des ToDos auf die höchste ToDo-ID dieses Users setzen!
+				// Die ID des ToDos auf die höchste ToDo-ID dieses Users setzen!
 				toDos = accounts.getUserFromToken(token).getToDos();
 				this.specificID = 0;
 				int toDosSize = toDos.size();
@@ -313,9 +315,8 @@ public class ServerModelClientThread extends Thread {
 					for (ToDo t : toDos) {
 						int id = t.getID();
 						if (id > this.specificID) {
-							this.specificID=id;
+							this.specificID = id;
 						}
-
 					}
 				}
 				specificID++;
